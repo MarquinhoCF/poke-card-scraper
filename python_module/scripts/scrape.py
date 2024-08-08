565124d365b580d49c9ba4b68f675f9a8a25c266
Marquinho <marcos13cf@gmail.com>
2024-08-08 16:46:14 -0300

    Refatoração do código de scrape dados de Pokemon TCG

--- python_module/scripts/scrape.py
+++ python_module/scripts/scrape.py
@@ -1,10 +1,17 @@
+from sched import scheduler
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
-import requests
-import os
+import json
+import re
+
+# VARIAVEIS IMPORTANTES
+# Caminho do arquivo onde o JSON será salvo
+output_file = 'pokemon_tcg_data.json'
+# URL para raspar
+url = 'https://www.tcgplayer.com/search/pokemon/product?view=list&productLineName=pokemon&setName=sv-scarlet-and-violet-151|swsh01-sword-and-shield-base-set|sm-base-set|xy-base-set|legendary-collection|xy-evolutions&page=1&inStock=true&Language=English'
 
 # Configurar as opções do Chrome
 options = Options()
@@ -30,115 +37,184 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 
 # FUNÇÕES DE EXTRAÇÃO DE DADOS
 def get_page_count():
-    pagination = driver.find_element(By.CSS_SELECTOR, ".tcg-pagination__pages")
-    page_links = pagination.find_elements(By.TAG_NAME, "a")
-    return int(page_links[-1].text)
-
-def extract_text(element, class_name):
     try:
-        el = element.find_element(By.CLASS_NAME, class_name)
+        pagination = driver.find_element(By.CSS_SELECTOR, ".tcg-pagination__pages")
+        page_links = pagination.find_elements(By.TAG_NAME, "a")
+        return int(page_links[-1].text)
+    except Exception as e:
+        print(f"Erro ao obter o número de páginas: {e}")
+        return 1  # Default para 1 página se não conseguir determinar
+
+def extract_text(element, class_name, by = By.CLASS_NAME):
+    try:
+        el = element.find_element(by, class_name)
         return el.text
-    except:
+    except Exception as e:
+        print(f"Erro ao extrair texto de '{class_name}' por '{by}': {e}")
         return None
 
 def extract_title(element):
-    return extract_text(element, "product-card__title")
+    return extract_text(element, "product-info__title")
 
-def extract_set(element):
-    return extract_text(element, "product-card__set-name__variant")
+def extract_meta(element):
+    meta_text = extract_text(element, "product-info__meta")
+    if meta_text:
+        parts = meta_text.split(", ")
+        set_name = parts[1].strip() if len(parts) > 1 else None
+        rarity = parts[2].strip() if len(parts) > 2 else None
+        number = parts[-1].strip() if "#" in parts[-1] else None
+        return set_name, rarity, number
+    else:
+        return None, None, None
 
 def extract_market_price(element):
-    return extract_text(element, "product-card__market-price--value")
-
-def extract_rarity(element):
-    rarity_text = extract_text(element, "product-card__rarity__variant")
-    if rarity_text:
-        return rarity_text.split("·")[0].strip()
-    return None
-
-def extract_number(element):
-    rarity_text = extract_text(element, "search-result__rarity")
-    if rarity_text:
-        parts = rarity_text.split("·")
-        return parts[1].strip() if len(parts) > 1 else None
-    return None
-
-def extract_img(element):
-    img_element = element.find_element(By.TAG_NAME, "img")
-    img_url = img_element.get_attribute("src")
-    return img_url.replace("/filters:quality(1)", "")
+    return extract_text(element, "product-info__market-price--value")
 
 def extract_product_url(element):
-    link_element = element.find_element(By.TAG_NAME, "a")
-    return link_element.get_attribute("href")
+    try:
+        link_element = element.find_element(By.TAG_NAME, "a")
+        return link_element.get_attribute("href")
+    except Exception as e:
+        print(f"Erro ao extrair URL do produto: {e}")
+        return None
 
 def extract_top_listings(element):
-    listings = element.find_elements(By.CLASS_NAME, "listing-item__info")
     top_listings = []
-    for listing in listings:
-        price = extract_text(listing, "listing-item__price")
-        shipping = extract_text(listing, "shipping-messages__price")
-        top_listings.append((price, shipping))
+    try:
+        listings = element.find_elements(By.CLASS_NAME, "listing-item__listing-data")
+        for listing in listings:
+            condition = extract_text(listing, "h3", By.TAG_NAME)
+            seller = extract_text(listing, "seller-info__name")
+            price = extract_text(listing, "listing-item__listing-data__info__price")
+            shipping = extract_text(listing, "shipping-messages__price")
+            if shipping:
+                shipping = shipping.split(" ")[1].strip()
+            top_listings.append({
+                "condition": condition,
+                "seller": seller,
+                "price": price.replace('$', ''),
+                "shipping": shipping.replace('$', '')
+            })
+    except Exception as e:
+        print(f"Erro ao extrair listagens: {e}")
     return top_listings
 
-# FUNÇÕES DE SALVAMENTO
-def save_img(img_url):
-    response = requests.get(img_url)
-    filename = os.path.join(image_dir, os.path.basename(img_url))
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-    return img_url
+# Salvar os dados em formato JSON formatado
+def save_data(data, filename=output_file):
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"Dados salvos em {filename}")
+    except Exception as e:
+        print(f"Erro ao salvar os dados em JSON: {e}")
 
-def extract_and_save_img(element):
-    img_url = extract_img(element)
-    save_img(img_url)
-    return img_url
+# Carregar os dados de um arquivo JSON
+def openJSONFile(filename=output_file):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
 
-# RASPAGEM E PROCESSAMENTO
+# INFORMAÇÕES ESTATÍSTICAS SOBRE O DATASET OBTIDO:
+def getStatistcs(dataset):
+    print("\n\n------------------- Análise estatística dos dados obtidos: -------------------\n")
+    # Número total de produtos observados
+    total_products = len(dataset)
+    print(f"Total de produtos observados: {total_products}")
 
-url = 'https://www.tcgplayer.com/search/pokemon/product?view=grid&productLineName=pokemon&setName=sv-scarlet-and-violet-151|world-championship-decks&page=1&inStock=true'
+    # Encontrar o preço de mercado máximo e mínimo
+    market_prices = []
+    for item in dataset:
+        try:
+            price = float(item['market_price']) if item['market_price'] else None
+            if price is not None:
+                market_prices.append(price)
+        except ValueError:
+            continue
 
-all_data = []
+    if market_prices:
+        max_market_price = max(market_prices)
+        min_market_price = min(market_prices)
+        print(f"Preço de mercado máximo: ${max_market_price}")
+        print(f"Preço de mercado mínimo: ${min_market_price}")
+    else:
+        print("Nenhum preço de mercado válido encontrado.")
 
-driver.get(url)
-time.sleep(20)
+    # Contagem de ocorrências de "sets"
+    set_counts = {}
+    for item in dataset:
+        set_name = item.get('set', None)
+        if set_name:
+            set_counts[set_name] = set_counts.get(set_name, 0) + 1
 
-results = driver.find_elements(By.CSS_SELECTOR, ".search-result__content")
+    print("\nOcorrências dos 'sets' observados:")
+    for set_name, count in set_counts.items():
+        print(f"{set_name}: {count} ocorrência(s)")
 
-print(results)
-for result in results:
-    data = {
-        'title': extract_title(result),
-        'set': extract_set(result),
-        'rarity': extract_rarity(result),
-        'number': extract_number(result),
-        'market_price': extract_market_price(result),
-        'product_url': extract_product_url(result),
-        'top_listings': extract_top_listings(result)
-    }
-    all_data.append(data)
+    # Contagem de ocorrências de "rarity"
+    rarity_counts = {}
+    for item in dataset:
+        rarity = item.get('rarity', None)
+        if rarity:
+            rarity_counts[rarity] = rarity_counts.get(rarity, 0) + 1
 
-print(all_data)
+    print("\nOcorrências dos 'rarity' observados:")
+    for rarity, count in rarity_counts.items():
+        print(f"{rarity}: {count} ocorrência(s)")
 
+    # Contagem de ocorrências de "condition"
+    condition_counts = {}
+    for item in dataset:
+        top_listings = item.get('top_listings', [])
+        for listing in top_listings:
+            condition = listing.get('condition', None)
+            if condition:
+                condition_counts[condition] = condition_counts.get(condition, 0) + 1
 
-# for page in range(1, get_page_count() + 1):
-#     driver.get(f"{url}&page={page}")
-#     time.sleep(2)  # Esperar a página carregar
+    print("\nOcorrências dos 'condition' observados:")
+    for condition, count in condition_counts.items():
+        print(f"{condition}: {count} ocorrência(s)")
 
-#     results = driver.find_elements(By.CSS_SELECTOR, ".search-result__content")
-#     for result in results:
-#         data = {
-#             'img_url': extract_and_save_img(result),
-#             'title': extract_title(result),
-#             'set': extract_set(result),
-#             'rarity': extract_rarity(result),
-#             'number': extract_number(result),
-#             'market_price': extract_market_price(result),
-#             'product_url': extract_product_url(result),
-#             'top_listings': extract_top_listings(result)
-#         }
-#         all_data.append(data)
+# Job de RASPAGEM E PROCESSAMENTO
+def job():
+    print("Iniciando a raspagem de dados de Pokemon TCG...")
+    all_data = []
 
-driver.quit()
+    try:
+        driver.get(url)
+        time.sleep(10) # Aguardar a página carregar completamente
 
-# Processar ou salvar all_data conforme necessário
+        num_pages = get_page_count()
+
+        for page in range(1, num_pages + 1):
+            print(f"Extraindo dados da página {page} de {num_pages} páginas...")
+            driver.get(re.sub(r'page=\d+', f'page={page}', url))
+            time.sleep(5) # Aguardar a página carregar completamente
+
+            results = driver.find_elements(By.CSS_SELECTOR, ".search-result__content")
+            for result in results:
+                try:
+                    set_name, rarity, number = extract_meta(result)
+                    data = {
+                        'title': extract_title(result),
+                        'set': set_name,
+                        'rarity': rarity,
+                        'number': number,
+                        'market_price': extract_market_price(result).replace('$', ''),
+                        'product_url': extract_product_url(result),
+                        'top_listings': extract_top_listings(result)
+                    }
+                    print(data)
+                    all_data.append(data)
+                except Exception as e:
+                    print(f"Erro ao processar resultado: {e}")
+
+    finally:
+        driver.quit()
+
+    save_data(all_data)
+    dataset = openJSONFile()
+    getStatistcs(dataset)
+
+while True:
+    scheduler.every().day.at("09:00").do(job)
+    scheduler.run_pending()
+    time.sleep(60)  # Verificar a cada minuto se é hora de executar o job
